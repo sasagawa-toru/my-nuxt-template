9bac863c05fe9b5e20b777ab00546d65e2a38689
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2022-12-05 18:58:27 +0900

    基本ロジックを実装

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="pending">Loading...</div>
+    <div v-else>
+      <v-list>
+        <v-list-item
+          v-for="user in data"
+          :key="user.userId"
+          :to="`/users/${user.userId}`"
+          >{{ user.name }}</v-list-item
+        >
+      </v-list>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { User } from "@/assets/models/User"
+
+const { pending, data } = useFetch<User[]>("/api/users")
+</script>
---
+++ pages/users/[userId].vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="pending || !data">Loading...</div>
+    <div v-else>
+      <div>
+        <span>名前: </span><span>{{ data.name }}</span>
+      </div>
+      <div>
+        <span>学籍番号: </span><span>{{ data.studentId }}</span>
+      </div>
+      <div>
+        <span>在籍寮: </span><span>{{ data.dormName }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { UserDetail } from "@/assets/models/UserDetail"
+
+// URLからユーザーIDを取得
+const route = useRoute()
+const userId = route.params.userId
+
+const { pending, data } = useFetch<UserDetail>(`/api/users/${userId}`)
+</script>
